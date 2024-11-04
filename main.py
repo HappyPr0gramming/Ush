95dc8f8413a428c233beffee6c945e6d52b33bc0
HappyPr0gramming <156795708+HappyPr0gramming@users.noreply.github.com>
2024-11-04 14:48:26 +0100

    Update main.py
    We finally have the built in text editor: Giga!

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 import platform
 
+
 osname = platform.system()
 osuser = os.getlogin()
 directory = os.getcwd()
@@ -100,6 +101,64 @@ while True:
         except PermissionError:
             print('You do not have the necessary permissions to execute that file')
         recognised = True
+
+    if 'giga' in command:
+        recognised = True
+        filename = command.replace('giga', '').strip()
+        def get_input():
+
+                if platform.system() == 'Windows':
+                    os.system('cls')
+                elif platform.system() == 'Darwin':
+                    os.system('clear')
+                elif platform.system() == 'Linux':
+                    os.system('clear')
+
+                indentvers = os.get_terminal_size().columns - 49
+                indentvers2 = indentvers // 2
+
+                print('-'*os.get_terminal_size().columns)
+                print('|',' '*indentvers2,'SatsumaProject Giga Text Editor Version 1.2',' '*indentvers2,'|')
+                print('-'*os.get_terminal_size().columns)
+
+                print('|')
+
+                lines = []
+
+                while True:
+                    line = input('|')
+                    if line.strip() == '#END':
+                        break
+                    elif line.strip() == '#DEL':
+                        if lines:
+                            lines.pop()
+                            try:
+                                print("\033[F\033[K", end="")
+                                print("\033[F\033[K", end="")
+                            except:
+                                print('Last Line Has Been Removed')
+                    else:
+                        lines.append(line)
+
+                return lines
+
+        indentvers = os.get_terminal_size().columns - 9
+        indentvers2 = indentvers // 2
+
+        text = get_input()
+
+        for x in range(3):
+
+            print('')
+
+        print('-'*os.get_terminal_size().columns)
+        file = filename
+        print(' '*indentvers2+'File Saved As '+file)
+
+        with open(file, 'w') as file:
+            for line in text:
+                file.write(line + '\n')        
+
 
 
     if recognised == False:
